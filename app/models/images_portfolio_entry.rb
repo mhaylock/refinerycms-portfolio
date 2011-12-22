0867d28e972cfa387fa114f041ba3be6722fe6e3
Mark Haylock <mark@resolvedigital.co.nz>
2011-12-22 15:31:05 +1300

    Add translations to portfolio image caption.

--- app/models/images_portfolio_entry.rb
+++ app/models/images_portfolio_entry.rb
@@ -3,6 +3,11 @@ class ImagesPortfolioEntry < ActiveRecord::Base
   belongs_to :image
   belongs_to :portfolio_entry
 
+  translates :caption if self.respond_to?(:translates)
+
+  attr_accessible :image_id, :position, :locale
+  ::ImagePage::Translation.send :attr_accessible, :locale
+
   before_save do |image_portfolio_entry|
     image_portfolio_entry.position = (ImagesPortfolioEntry.maximum(:position) || -1) + 1
   end
